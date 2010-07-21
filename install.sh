48877262f1d5c566f94e24c9af900856fafe81f7
thisirs <thisirs@gmail.com>
2010-07-21 16:46:07 +0200

    install.sh downloads anything and org repo

--- install.sh
+++ install.sh
@@ -1,8 +1,22 @@
 #! /usr/bin/env bash
 
-[ -e "$HOME/.emacs" ] && mv $HOME/.emacs{,.old}
-[ -e "$HOME/.emacs.d" ] && mv $HOME/.emacs.d{,.old}
-GIT_PATH=$(pwd)
+INSTALL_PATH=~/git
+
+#Création de la structure
+mkdir -p "$INSTALL_PATH/dotemacs"
+mv .emacs.d "$INSTALL_PATH/dotemacs"
+mv .emacs "$INSTALL_PATH/dotemacs"
+
+#Téléchargement des derniers paquets
+cd "$INSTALL_PATH"
+git clone git://repo.or.cz/anything-config.git
+git clone git://repo.or.cz/org-mode.git
+
+#Sauvegarde de la configuration existante
+[ -e "$HOME/.emacs" ] && {echo "Moving existing ~/.emacs to ~/.emacs.old"; mv $HOME/.emacs{,.old}}
+[ -e "$HOME/.emacs.d" ] && {echo "Moving existing ~/.emacs.d to ~/.emacs.d.old"; mv $HOME/.emacs.d{,.old}}
+
+#Création des liens
 cd $HOME
-ln -s $GIT_PATH/.emacs
-ln -s $GIT_PATH/.emacs.d
+ln -s "$INSTALL_PATH/dotemacs/.emacs"
+ln -s "$INSTALL_PATH/dotemacs/.emacs.d"
