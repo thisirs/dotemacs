485e3e0f760cd4305870fac2368ac72fac0c4ff9
thisirs <thisirs@gmail.com>
2010-09-23 17:52:11 +0200

    rewrite notify-send (to test)

--- .emacs.d/notify-send.el
+++ .emacs.d/notify-send.el
@@ -6,21 +6,22 @@
   "/usr/share/icons/gnome/scalable/status/appointment-missed.svg"
   "Icone par défault pour notify-send")
 
-(defun notify-send (title msg &optional icon sound)
+(defun notify-send (title &optional msg icon sound)
   "Show a popup if we're on X, or echo it otherwise; TITLE is the title
 of the message, MSG is the context. Optionally, you can provide an ICON and
 a sound to be played"
   (interactive "sTitle: \nsMessage: ")
-  (when sound
-    (progn
-      (when (eq t sound) (setq sound notify-send-sound-default))
-      (shell-command (concat "mplayer " sound " 2> /dev/null"))))
-  (when (eq window-system 'x)
-    (when (eq t icon) (setq icon notify-send-icon-default))
-    (shell-command (concat "notify-send "
-		     (if icon (concat "-i " icon) "")
-                     " '" title "' '" msg "'")))
-    ;; text only version
-    (message (concat title ": " msg)))
+  (and sound
+    (shell-command (concat
+		     "mplayer "
+		     (if (stringp sound)
+		       sound notify-send-sound-default)
+		     " 2>&1 /dev/null") nil nil))
+  (or (stringp msg) (setq msg title))
+  (and (eq window-system 'x)
+    (shell-command (concat "notify-send -i "
+		     (if (stringp icon)
+		       icon notify-send-icon-default)
+		     " '" title "' '" msg "'")) nil nil))
 
 (provide 'notify-send)
