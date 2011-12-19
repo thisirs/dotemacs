ebe56e6f722ccc608c1c1fa28f3c08a5eb7da00b
Maths <maths@quark.2003r2-math.prive>
2011-12-19 17:02:39 +0100

    update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,25 @@
 #! /usr/bin/env bash
 
-INSTALL_PATH=${1:-"/$HOME/git-repos"}
+# Full path of script
+SCRIPT_PATH=$(readlink -f "$(dirname "$0")")
 
+# Where to copy dotemacs repo
+INSTALL_PATH=${1:-"$HOME/git-repos"}
+
+mkdir -p $INSTALL_PATH
 cd $INSTALL_PATH
-git clone git@github.com:thisirs/dotemacs.git
+cp -r $SCRIPT_PATH $INSTALL_PATH
+
+cd dotemacs
+
 git submodule init
 git submodule update
 
 #Sauvegarde de la configuration existante
-[ -e "$HOME/.emacs" ] && { echo "Moving existing ~/.emacs to ~/.emacs.old"; mv $HOME/.emacs{,.old}; }
-[ -e "$HOME/.emacs.d" ] && { echo "Moving existing ~/.emacs.d to ~/.emacs.d.old"; mv $HOME/.emacs.d{,.old}; }
+[ -e "$HOME/.emacs" ] && { echo "Moving existing ~/.emacs to ~/.emacs-$(date +%s).old"; mv $HOME/.emacs{,-$(date +%s).old}; }
+[ -e "$HOME/.emacs.d" ] && { echo "Moving existing ~/.emacs.d to ~/.emacs.d-$(date +%s).old"; mv $HOME/.emacs.d{,-$(date +%s).old}; }
 
 #Création des liens
 cd $HOME
-ln -s "$INSTALL_PATH/dotemacs/.emacs"
 ln -s "$INSTALL_PATH/dotemacs/.emacs.d"
 
