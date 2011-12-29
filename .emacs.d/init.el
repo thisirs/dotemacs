a7fcf38a6039723cc467e6f4b286f412776da38d
Maths <maths@quark.2003r2-math.prive>
2011-12-29 23:54:24 +0100

    yasnippet inner field navigation + C-k

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1035,6 +1035,32 @@ when building sentence like blah, blih, bloh and bluh."
 (yas/global-mode 1)
 (setq yas/triggers-in-field t)
 
+;; Inter-field navigation
+(defun yas/goto-end-of-active-field ()
+  (interactive)
+  (let* ((snippet (car (yas/snippets-at-point)))
+         (position (yas/field-end (yas/snippet-active-field snippet))))
+    (goto-char position)))
+
+(defun yas/goto-start-of-active-field ()
+  (interactive)
+  (let* ((snippet (car (yas/snippets-at-point)))
+         (position (yas/field-start (yas/snippet-active-field snippet))))
+    (goto-char position)))
+
+(define-key yas/keymap (kbd "C-e") 'yas/goto-end-of-active-field)
+(define-key yas/keymap (kbd "C-a") 'yas/goto-start-of-active-field)
+
+;; C-k in a field
+(defun yas/clear-current-field ()
+  (interactive)
+  (let ((field (and yas/active-field-overlay
+                    (overlay-buffer yas/active-field-overlay)
+                    (overlay-get yas/active-field-overlay 'yas/field))))
+    (and field (delete-region (point) (yas/field-end field)))))
+
+(define-key yas/keymap (kbd "C-k") 'yas/clear-current-field)
+
 ;;; org-mode
 (require 'org)
 
