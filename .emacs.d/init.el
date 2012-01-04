7b07abecc794caf8d6f41b7973c4b67e48cb4cd2
thisirs <thisirs@gmail.com>
2012-01-04 16:07:10 +0100

    rewrote ibuffer advice to switch to most recent buffer
    using `ibuffer-next-buffer'

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -548,12 +548,9 @@ when building sentence like blah, blih, bloh and bluh."
 
 (define-key ibuffer-mode-map (kbd "C-g") 'ibuffer-quit)
 
-(defadvice ibuffer (around ibuffer-point-to-most-recent) ()
+(defadvice ibuffer (after ibuffer-point-to-most-recent activate) ()
   "Open ibuffer with cursor pointed to most recent buffer name"
-  (let ((recent-buffer-name (buffer-name (other-buffer))))
-    ad-do-it
-    (ibuffer-jump-to-buffer recent-buffer-name)))
-(ad-activate 'ibuffer)
+    (ibuffer-next-buffer))
 
 (defun find-projects (dir)
   "Return a list of all directories containing a not hidden git repo"
