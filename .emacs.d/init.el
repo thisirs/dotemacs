7317deb78824cc8edb9b43246e85f8eb6dbcbebb
thisirs <thisirs@gmail.com>
2012-01-10 12:15:45 +0100

    activate in ibuffer advice

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -510,15 +510,13 @@ when building sentence like blah, blih, bloh and bluh."
 
 (define-key ibuffer-mode-map (kbd "C-g") 'ibuffer-quit)
 
-(defadvice ibuffer (around ibuffer-point-to-most-recent)
+(defadvice ibuffer (around ibuffer-point-to-most-recent activate)
   "Open ibuffer with cursour pointed to second most recent buffer
 name"
   (let ((recent-buffer-name (buffer-name)))
     ad-do-it
     (ibuffer-jump-to-buffer recent-buffer-name)
     (ibuffer-next-buffer)))
-
-(ad-activate 'ibuffer)
 
 (defun find-projects (dir)
   "Return a list of all directories containing a not hidden git repo"
