fac09af3ec928bcb07f4936075ff12db167ae910
thisirs <thisirs@gmail.com>
2012-02-08 16:27:20 +0100

    improved boss key

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -201,6 +201,7 @@ Also returns nil if pid is nil."
 
 (defvar boss-window-configuration nil "Window configuration to switch to when the boss comes!")
 (defvar my-window-configuration nil "Window configuration to switch back to!")
+(defvar boss-mode nil)
 
 (defun boss-save nil
   "Define current window configuration as boss's one"
@@ -208,15 +209,20 @@ Also returns nil if pid is nil."
   (setq boss-window-configuration (current-window-configuration))
   (message "Boss window configuration set to current!"))
 
-(defun boss-reload nil
-  "Switch to boss windows configuration"
+(defun boss-toggle nil
+  "Toggle boss window configuration. Switch to *scratch* buffer
+if `boss-window-configuration' is nil."
   (interactive)
-  (if (equal (current-window-configuration) boss-window-configuration)
+  (if boss-mode
       (set-window-configuration my-window-configuration)
     (setq my-window-configuration (current-window-configuration))
-    (set-window-configuration boss-window-configuration)))
+    (if boss-window-configuration
+        (set-window-configuration boss-window-configuration)
+      (delete-other-windows)
+      (switch-to-buffer "*scratch*")))
+  (setq boss-mode (null boss-mode)))
 
-(global-set-key (kbd "²") 'boss-reload)
+(global-set-key (kbd "²") 'boss-toggle)
 (global-set-key (kbd "C-²") 'boss-save)
 
 
