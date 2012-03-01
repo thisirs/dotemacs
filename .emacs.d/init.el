90a086f10da4b2b03d54df1f49a6fd0579f34e3f
thisirs <thisirs@gmail.com>
2012-03-01 17:54:36 +0100

    no append needed here

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -975,19 +975,19 @@ name"
 ;; save a bunch of variables to the desktop file
 ;; for lists specify the len of the maximal saved data also
 (setq desktop-globals-to-save
-      (append '((extended-command-history . 30)
-                (file-name-history        . 100)
-                (grep-history             . 30)
-                (compile-history          . 30)
-                (minibuffer-history       . 50)
-                (query-replace-history    . 60)
-                (read-expression-history  . 60)
-                (regexp-history           . 60)
-                (regexp-search-ring       . 20)
-                (search-ring              . 20)
-                (shell-command-history    . 50)
-                tags-file-name
-                register-alist)))
+      '((extended-command-history . 30)
+        (file-name-history        . 100)
+        (grep-history             . 30)
+        (compile-history          . 30)
+        (minibuffer-history       . 50)
+        (query-replace-history    . 60)
+        (read-expression-history  . 60)
+        (regexp-history           . 60)
+        (regexp-search-ring       . 20)
+        (search-ring              . 20)
+        (shell-command-history    . 50)
+        tags-file-name
+        register-alist))
 
 (setq desktop-files-not-to-save
       "\\(^/[^/:]*:\\|(ftp)$\\)\\|\\(^/tmp/\\)\\|\\(.gpg$\\)")
