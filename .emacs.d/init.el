1f738fc4171be797ae472806ecfaed18fe28a569
thisirs <thisirs@gmail.com>
2012-03-07 16:42:13 +0100

    added latex-replace-by-closest

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2397,3 +2397,68 @@ shows you how many labels and refs have been replaced."
   )
 
 (run-with-idle-timer 900 t 'jump-to-org-agenda)
+
+
+(defun latex-replace-by-closest ()
+  "Replace filename in all includegraphics by closest filename
+with respect to Levenshtein distance. Candidates are found in the
+subdirectory img and filtered by extension."
+  (interactive)
+  (let ((img-files (directory-files "./img" nil
+                                    (regexp-opt '(".jpg" ".jpeg" ".png")))))
+    (save-excursion
+      (goto-char 1)
+      (while (re-search-forward "\\\\includegraphics\\(\\[[^]]+\\]\\)?{\\([^}]+\\)" nil t)
+        (let* ((old-filename (match-string-no-properties 2))
+               (closest "")
+               (new-filename (dolist (file img-files closest)
+                               (if (> (levenshtein-distance old-filename closest)
+                                      (levenshtein-distance old-filename
+                                                            (file-name-sans-extension file)))
+                                   (setq closest (file-name-sans-extension file))))))
+          (and
+           (> (length new-filename) 0)
+           (not (equal new-filename old-filename))
+           (yes-or-no-p
+            (format "Replace %s by %s? " old-filename new-filename))
+           (replace-match new-filename nil nil nil 2)))))))
+
+;; taken from http://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance#Emacs_Lisp
+(defun levenshtein-distance (str1 str2)
+  "Return the edit distance between strings STR1 and STR2."
+  (if (not (stringp str1))
+      (error "Argument was not a string: %s" str1))
+  (if (not (stringp str2))
+      (error "Argument was not a string: %s" str2))
+  (let* ((make-table (function (lambda (columns rows init)
+                                 (make-vector rows (make-vector columns init)))))
+         (tref (function (lambda (table x y)
+                           (aref (aref table y) x))))
+         (tset (function (lambda (table x y object)
+                           (let ((row (copy-sequence (aref table y))))
+                             (aset row x object)
+                             (aset table y row) object))))
+         (length-str1 (length str1))
+         (length-str2 (length str2))
+         (d (funcall make-table (1+ length-str1) (1+ length-str2) 0)))
+    (let ((i 0) (j 0))
+      (while (<= i length-str1)
+        (funcall tset d i 0 i)
+        (setq i (1+ i)))
+      (while (<= j length-str2)
+        (funcall tset d 0 j j)
+        (setq j (1+ j))))
+    (let ((i 1))
+      (while (<= i length-str1)
+        (let ((j 1))
+          (while (<= j length-str2)
+            (let* ((cost (if (equal (aref str1 (1- i)) (aref str2 (1- j)))
+                             0
+                           1))
+                   (deletion (1+ (funcall tref d (1- i) j)))
+                   (insertion (1+ (funcall tref d i (1- j))))
+                   (substitution (+ (funcall tref d (1- i) (1- j)) cost)))
+              (funcall tset d i j (min insertion deletion substitution)))
+            (setq j (1+ j))))
+        (setq i (1+ i))))
+    (funcall tref d length-str1 length-str2)))
