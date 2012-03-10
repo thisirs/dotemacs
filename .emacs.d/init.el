ddd0180b3edcc297705a0d2eb75f56e6b52b992a
sylvain <sylvain@Knuth.home>
2012-03-10 01:55:31 +0100

    fix bug when opening file /home/non-existing/blah.foo

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -45,7 +45,8 @@
 
 (defadvice find-file (around th-find-file activate)
   "Open FILENAME using tramp's sudo method if it's read-only."
-  (if (and (not (file-writable-p (ad-get-arg 0)))
+  (if (and (file-exists-p (ad-get-arg 0))
+           (not (file-writable-p (ad-get-arg 0)))
            (not (file-remote-p (ad-get-arg 0)))
            (not (file-directory-p (ad-get-arg 0)))
            (y-or-n-p (concat "File "
