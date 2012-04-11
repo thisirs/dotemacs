9fa3b69c76e5c100597462b090aded4dee642caa
thisirs <thisirs@gmail.com>
2012-04-11 14:51:12 +0200

    added electric return in elisp mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1886,7 +1886,25 @@ or version controlled but untracked."
           (lambda ()
             (local-set-key (kbd "C-x E") 'eval-region-or-buffer)))
 
+(defvar electrify-return-match
+  "[\]}\)\"]"
+  "If this regexp matches the text after the cursor, do an \"electric\"
+  return.")
 
+(defun electrify-return-if-match (arg)
+  "If the text after the cursor matches `electrify-return-match' then
+  open and indent an empty line between the cursor and the text.  Move the
+  cursor to the new line."
+  (interactive "P")
+  (let ((case-fold-search nil))
+    (if (looking-at electrify-return-match)
+        (save-excursion (newline-and-indent)))
+    (newline arg)
+    (indent-according-to-mode)))
+
+(add-hook 'emacs-lisp-mode-hook
+          (lambda ()
+            (local-set-key (kbd "RET") 'electrify-return-if-match)))
 
 (add-hook 'emacs-lisp-mode-hook
           (lambda()
