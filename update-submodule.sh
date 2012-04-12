c1b0219115ff6de05fc81c4c48a4ac280d4622e8
thisirs <thisirs@gmail.com>
2012-04-12 13:59:19 +0200

    added comment in update-submodule.sh

--- update-submodule.sh
+++ update-submodule.sh
@@ -1,9 +1,15 @@
 #! /bin/bash -
 
+# Updates all submodules. Submodules are mainly clones of github
+# repos. Some of them are clones of repos i forked in github to be
+# able to modify them easily. Therefore for each submodule i have a
+# origin repo and a upstream repo for those that are clones of forks
+# in github.
+
 # Checking out master in all submodule
 git submodule foreach git checkout master
 
-# Fetching and upstream if it exists
+# Fetching all remote repos and upstream if it exists
 git submodule foreach git fetch
 git submodule foreach "git fetch upstream || true"
 
