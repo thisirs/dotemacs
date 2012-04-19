bb7dd5256a3bc1442a433a2acb0ce280cac3fada
sylvain <sylvain@Knuth.home>
2012-04-19 23:57:40 +0200

    added (require 'package)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -952,6 +952,7 @@ or version controlled but untracked."
 
 
 ;; adding packages source
+(require 'package)
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (add-to-list 'package-archives
