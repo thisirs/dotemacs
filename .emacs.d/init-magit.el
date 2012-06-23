a6540d10b63c2ee09d096b7de7c9f3c41d5b4709
sylvain <sylvain@mint>
2012-06-23 02:22:46 +0200

    increase context line in magit

--- .emacs.d/init-magit.el
+++ .emacs.d/init-magit.el
@@ -7,7 +7,7 @@
 ;; look at diff when writing a commit message
 (defun magit-log-show-diff ()
   (interactive)
-  (let ((content (magit-cmd-output "git" '("diff" "--cached"))))
+  (let ((content (magit-cmd-output "git" '("diff" "--cached" "-U5"))))
     (with-current-buffer (get-buffer-create "*vc-diff*")
       (let ((buffer-undo-list t)
             (inhibit-read-only t))
