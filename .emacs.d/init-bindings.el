a7904fca2311cf6fe5f76b7e246677abca244684
thisirs <thisirs@gmail.com>
2012-06-25 18:07:22 +0200

    wrote create-flash-binding

--- .emacs.d/init-bindings.el
+++ .emacs.d/init-bindings.el
@@ -47,4 +47,49 @@
 
 (global-set-key [(control tab)] 'other-window)
 
+(defmacro create-flash-binding (key)
+  "Make key `key' boundable to a complex command. Select the
+complex command by typing C-`key'. Useful for example to repeat
+an eval from M-:. Reuses the code from `repeat-complex-command'."
+  `(global-set-key
+    (kbd ,(concat "C-" key))
+    (lambda ()
+      (interactive)
+      (lexical-let ((elt (nth 0 command-history))
+                    newcmd)
+        (if elt
+            (progn
+              (setq newcmd
+                    (let ((print-level nil)
+                          (minibuffer-history-position 0)
+                          (minibuffer-history-sexp-flag (1+ (minibuffer-depth))))
+                      (unwind-protect
+                          (read-from-minibuffer
+                           "Redo: " (prin1-to-string elt) read-expression-map t
+                           (cons 'command-history 0))
+
+                        ;; If command was added to command-history as a
+                        ;; string, get rid of that.  We want only
+                        ;; evaluable expressions there.
+                        (if (stringp (car command-history))
+                            (setq command-history (cdr command-history))))))
+
+              ;; If command to be redone does not match front of history,
+              ;; add it to the history.
+              (or (equal newcmd (car command-history))
+                  (setq command-history (cons newcmd command-history)))
+              (global-set-key
+               (kbd ,key)
+               (lambda ()
+                 (interactive)
+                 (eval newcmd))))
+          (if command-history
+              (error "Argument %d is beyond length of command history" 0)
+            (error "There are no previous complex commands to repeat")))))))
+
+(create-flash-binding "<f9>")
+(create-flash-binding "<f10>")
+(create-flash-binding "<f11>")
+(create-flash-binding "<f12>")
+
 (provide 'init-bindings)
