9b60e50772658212707603f42cafd2b90915287d
sylvain <sylvain@mint>
2012-06-30 19:36:42 +0200

    added paredit defuns

--- .emacs.d/init-elisp.el
+++ .emacs.d/init-elisp.el
@@ -89,4 +89,68 @@
 (add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
 (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
 
+;; From https://github.com/jwiegley/dot-emacs
+(eval-after-load "paredit"
+  '(progn
+     (defun paredit-barf-all-the-way-backward ()
+       (interactive)
+       (paredit-split-sexp)
+       (paredit-backward-down)
+       (paredit-splice-sexp))
+
+     (defun paredit-barf-all-the-way-forward ()
+       (interactive)
+       (paredit-split-sexp)
+       (paredit-forward-down)
+       (paredit-splice-sexp)
+       (if (eolp) (delete-horizontal-space)))
+
+     (defun paredit-slurp-all-the-way-backward ()
+       (interactive)
+       (catch 'done
+	 (while (not (bobp))
+	   (save-excursion
+	     (paredit-backward-up)
+	     (if (eq (char-before) ?\()
+		 (throw 'done t)))
+	   (paredit-backward-slurp-sexp))))
+
+     (defun paredit-slurp-all-the-way-forward ()
+       (interactive)
+       (catch 'done
+	 (while (not (eobp))
+	   (save-excursion
+	     (paredit-forward-up)
+	     (if (eq (char-after) ?\))
+		 (throw 'done t)))
+	   (paredit-forward-slurp-sexp))))
+
+     (nconc paredit-commands
+	    '("Extreme Barfage & Slurpage"
+	      (("C-M-)")
+	       paredit-slurp-all-the-way-forward
+	       ("(foo (bar |baz) quux zot)"
+		"(foo (bar |baz quux zot))")
+	       ("(a b ((c| d)) e f)"
+		"(a b ((c| d)) e f)"))
+	      (("C-M-}")
+	       paredit-barf-all-the-way-forward
+	       ("(foo (bar |baz quux) zot)"
+		"(foo (bar|) baz quux zot)"))
+	      (("C-M-(")
+	       paredit-slurp-all-the-way-backward
+	       ("(foo bar (baz| quux) zot)"
+		"((foo bar baz| quux) zot)")
+	       ("(a b ((c| d)) e f)"
+		"(a b ((c| d)) e f)"))
+	      (("C-M-{")
+	       paredit-barf-all-the-way-backward
+	       ("(foo (bar baz |quux) zot)"
+		"(foo bar baz (|quux) zot)"))))
+
+     (paredit-define-keys)
+     (paredit-annotate-mode-with-examples)
+     (paredit-annotate-functions-with-examples)))
+
 (provide 'init-elisp)
+  
