be27f62731b7f091de5e4482ab75913393f557ee
thisirs <thisirs@gmail.com>
2012-07-06 16:59:00 +0200

    updated install.sh

--- install.sh
+++ install.sh
@@ -3,23 +3,15 @@
 # Full path of script
 SCRIPT_PATH=$(readlink -f "$(dirname "$0")")
 
-# Where to copy dotemacs repo
-INSTALL_PATH=${1:-"/$HOME/repositories"}
-
-
-mkdir -p $INSTALL_PATH
-cd $INSTALL_PATH
-cp -r $SCRIPT_PATH $INSTALL_PATH
-
-cd dotemacs
-
+# Init and update submodules
+cd $SCRIPT_PATH
 git submodule init
 git submodule update
 
-#Sauvegarde de la configuration existante
+# Backup existing .emacs and .emacs.d
 [ -e "$HOME/.emacs" ] && { echo "Moving existing ~/.emacs to ~/.emacs-$(date +%s).old"; mv $HOME/.emacs{,-$(date +%s).old}; }
 [ -e "$HOME/.emacs.d" ] && { echo "Moving existing ~/.emacs.d to ~/.emacs.d-$(date +%s).old"; mv $HOME/.emacs.d{,-$(date +%s).old}; }
 
-#Création des liens
-cd $HOME
-ln -s "$INSTALL_PATH/dotemacs/.emacs.d"
+# Create soft link
+ln -s -t ~/ $SCRIPT_PATH
+
