b902cf6b73b5c480e51b48c423170dc2bef9305a
thisirs <thisirs@gmail.com>
2012-10-31 18:28:37 +0100

    Add delim snippet for latex-mode

---
+++ snippets/latex-mode/.yas-setup.el
@@ -0,0 +1,17 @@
+(defun yas/maybe-escape (text)
+  (let ((first-char (if (> (length text) 0) (substring text 0 1))))
+    (cond
+     ((equal first-char "{") "\\{"))))
+
+(defun yas/corresponding-delim (text)
+  (let ((first-char (if (> (length text) 0) (substring text 0 1)))
+        (second-char (if (> (length text) 1) (substring text 1 2))))
+    (cond
+     ((equal first-char "(") ")")
+     ((equal first-char "[") "]")
+     ((equal first-char "{") "}")
+     ((equal first-char "\\")
+      (cond
+       ((equal second-char "(") ")")
+       ((equal second-char "[") "]")
+       ((equal second-char "{") "\\}"))))))
