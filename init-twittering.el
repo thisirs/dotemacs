451d93a67fc87c6d5eba948ee74cdaffd46e415a
thisirs <thisirs@gmail.com>
2012-11-01 23:43:26 +0100

    init-twittering.el: Add an open link feature to twittering-mode like org-mode's agenda one

--- init-twittering.el
+++ init-twittering.el
@@ -1,3 +1,19 @@
 (setq twittering-use-master-password t)
 
+(defun twittering-enter-next-uri ()
+  (interactive)
+  (let ((p (point))
+        (bound (point-at-eol)))
+    (twittering-goto-next-uri)
+    (when (and (not (eq (point) p))
+               (< (point) bound))
+      (twittering-enter))
+    (goto-char p)))
+
+(define-key twittering-mode-map (kbd "C-c C-o") 'twittering-enter-next-uri)
+
+;; (add-hook 'twittering-mode-hook
+;;           (lambda ()
+;;             (twittering-icon-mode 1)))
+
 (provide 'init-twittering)
