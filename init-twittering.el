c70d8a980e80026704528a380f1720201820ca4e
thisirs <thisirs@gmail.com>
2012-11-03 11:42:15 +0100

    Add twittering settings

--- init-twittering.el
+++ init-twittering.el
@@ -1,4 +1,5 @@
 (setq twittering-use-master-password t)
+(setq twittering-icon-mode t)
 
 (defun twittering-enter-next-uri ()
   (interactive)
@@ -10,10 +11,22 @@
       (twittering-enter))
     (goto-char p)))
 
-(define-key twittering-mode-map (kbd "C-c C-o") 'twittering-enter-next-uri)
+(add-hook
+ 'twittering-mode-hook
+ (lambda ()
+   (define-key twittering-mode-map (kbd "C-c C-o") 'twittering-enter-next-uri)
+   (define-key twittering-mode-map (kbd "C-c C-q") 'twittering-kill-buffer)
+   (define-key twittering-mode-map (kbd "q") 'bury-buffer)))
 
-;; (add-hook 'twittering-mode-hook
-;;           (lambda ()
-;;             (twittering-icon-mode 1)))
+(add-hook 'twittering-new-tweets-hook
+          (lambda ()
+            (let ((n twittering-new-tweets-count)))
+            (when (and (> n 10)
+                       (require 'notifications nil t))
+              (notifications-notify
+               :title
+               (twittering-timeline-spec-to-string twittering-new-tweets-spec)
+               :body
+               (format "You have %d new tweet%s" n (if (> n 1) "s" ""))))))
 
 (provide 'init-twittering)
