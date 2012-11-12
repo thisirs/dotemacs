a357ed2c76145209fd9eb40352f120bcd3694ae2
thisirs <thisirs@gmail.com>
2012-11-12 18:10:44 +0100

    init-bindings.el: Experimental shortcut

--- init-bindings.el
+++ init-bindings.el
@@ -19,43 +19,43 @@
 (global-set-key (kbd "s-n") 'windmove-down)
 
 ;; quick jump to useful buffers
-(defmacro shortcut (keybinding buffer-file &rest body)
-  "Make KEYBINDING open buffer or file BUFFER-FILE. If that
+(defmacro shortcut (keybinding target &rest body)
+  "Make KEYBINDING open buffer or file TARGET. If that
 buffer or file does not exist, ask confirmation before executing
 BODY if it is non nil."
-  (declare (debug defun))
+  (declare (debug t))
   `(global-set-key
     (kbd ,keybinding)
     (lambda ()
       (interactive)
-      (let* ((buf (get-buffer ,buffer-file))
-             (file (and (stringp ,buffer-file)
-                        (file-name-absolute-p ,buffer-file)
-                        (file-truename ,buffer-file)))
-             (current (if buf (current-buffer)
-                        (if buffer-file-name
-                            (file-truename
-                             (expand-file-name
-                              buffer-file-name)))))
-             (target (or buf file)))
+      (let (form-goto form-return)
         (cond
-         ((not target)
-          (if (not (and ',body (yes-or-no-p "Non-existent target. Execute form? ")))
-              (error "Non-existent target")
-            ,@body))
-         ((equal target current)
-          (jump-to-register ?x))
+         ((stringp ,target)
+          (if (file-name-absolute-p ,target)
+              (setq form-goto '(find-file (file-truename ,target))
+                    form-return '(equal target-name (file-truename ,target)))
+            (setq form-goto '(switch-to-buffer (get-buffer ,target))
+                  form-return '(eq (current-buffer) (get-buffer ,target)))))
+         ((not (listp (cdr ,target)))
+          (setq form-goto (car ,target)
+                form-return (cdr ,target)))
          (t
+          (setq form-goto ,target)))
+        (if (eval form-return)
+            (jump-to-register ?x)
           (window-configuration-to-register ?x)
-          (if buf
-              (switch-to-buffer buf)
-            (find-file (file-truename file)))))))))
+          (unless (eval form-goto)
+            (if ',body
+                (if (yes-or-no-p "Non-existent target. Execute form? ")
+                    (progn ,@body))
+              (error "Non-existent target"))))))))
 
 (shortcut "s-s s" "*scratch*")
 (shortcut "s-s e" "~/.emacs.d/init.el")
 (shortcut "s-s m" "*Messages*")
 (shortcut "s-s t" ":home" (twit))
 (shortcut "s-s g" "*Group*" (gnus))
+(shortcut "s-s i"  (erc-track-switch-buffer 1) (erc-start-or-switch 1))
 
 (global-set-key (kbd "C-x à") 'delete-other-windows)
 (global-set-key (kbd "C-x C-à") 'delete-other-windows)
