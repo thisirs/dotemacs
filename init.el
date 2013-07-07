43d1b90502b65548b6d2d7708668227953e33e28
thisirs <thisirs@gmail.com>
2013-07-07 23:07:05 +0200

    init.el: Remove marmalade

--- init.el
+++ init.el
@@ -28,8 +28,6 @@
 (with-emacs-version>= "24"
   (require 'package)
   (add-to-list 'package-archives
-               '("marmalade" . "http://marmalade-repo.org/packages/") t)
-  (add-to-list 'package-archives
                '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
   (package-initialize)
