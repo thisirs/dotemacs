e9c46ce2900d84995a05664f69dc84b671042f0b
thisirs <thisirs@gmail.com>
2013-07-27 20:08:50 +0200

    Autoinsert URL in elisp header files

--- snippets/emacs-lisp-mode/.yas-setup.el
+++ snippets/emacs-lisp-mode/.yas-setup.el
@@ -1,4 +1,6 @@
 (defun yas/wordify (s)
+  "Transform a mail adress by replacing \".\" by \" dot \" and
+\"@\" by \" at \"."
   (let ((rep '(("@" . " at ")
                ("\\." . " dot "))))
     (mapc
@@ -6,3 +8,19 @@
        (setq s (replace-regexp-in-string (car e) (cdr e) s)))
      rep)
     s))
+
+(defun yas/insert-url-maybe ()
+  (let ((backend
+         (catch 'found
+           (dolist (backend vc-handled-backends)
+             (let ((path (vc-call-backend backend 'responsible-p default-directory)))
+               (if path (throw 'found backend)))))))
+    (if backend
+        (concat ";; URL: "
+                (cond
+                 ((eq 'Git backend)
+                  (with-temp-buffer
+                    (vc-git-command t nil nil "config" "--get" "remote.origin.url")
+                    (buffer-string)))
+                 (t "")))
+      "")))
