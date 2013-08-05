646ae0197d5b1ff4e11d1f64fa38e773ea3a1a1e
thisirs <thisirs@gmail.com>
2013-08-05 18:05:25 +0200

    Set default languages for google translate

--- init.el
+++ init.el
@@ -195,6 +195,10 @@
 (global-set-key "\C-ct" 'google-translate-at-point)
 (global-set-key "\C-cT" 'google-translate-query-translate)
 
+(setq google-translate-default-source-language "en")
+(setq google-translate-default-target-language "fr")
+
+
 ;; Markdown
 (autoload 'markdown-mode "markdown-mode")
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
