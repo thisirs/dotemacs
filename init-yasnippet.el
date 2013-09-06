c5822ecfb884053b03a36afa01338e11eee6acb1
thisirs <thisirs@gmail.com>
2013-09-06 21:19:12 +0200

    init-yasnippet.el: Rename obsolete variables

--- init-yasnippet.el
+++ init-yasnippet.el
@@ -1,44 +1,43 @@
 (require 'yasnippet)
 
 ;; Use only own snippets, do not use bundled ones
-(setq yas/snippet-dirs '("~/.emacs.d/snippets"))
+(setq yas-snippet-dirs '("~/.emacs.d/snippets"))
 
-(yas/global-mode 1)
+(yas-global-mode 1)
 
-(setq yas/triggers-in-field t)
+(setq yas-triggers-in-field t)
 
-(if (boundp 'yas-new-snippet-default)
-    (setq yas-new-snippet-default "\
+(setq yas-new-snippet-default "\
 # -*- mode: snippet -*-
 # name: $1
 # --
-$0"))
+$0")
 
 ;; Inter-field navigation
-(defun yas/goto-end-of-active-field ()
+(defun yas-goto-end-of-active-field ()
   (interactive)
-  (let* ((snippet (car (yas/snippets-at-point)))
-         (position (yas/field-end (yas/snippet-active-field snippet))))
+  (let* ((snippet (car (yas--snippets-at-point)))
+         (position (yas--field-end (yas--snippet-active-field snippet))))
     (goto-char position)))
 
-(defun yas/goto-start-of-active-field ()
+(defun yas-goto-start-of-active-field ()
   (interactive)
-  (let* ((snippet (car (yas/snippets-at-point)))
-         (position (yas/field-start (yas/snippet-active-field snippet))))
+  (let* ((snippet (car (yas--snippets-at-point)))
+         (position (yas--field-start (yas--snippet-active-field snippet))))
     (goto-char position)))
 
-(define-key yas/keymap (kbd "C-e") 'yas/goto-end-of-active-field)
-(define-key yas/keymap (kbd "C-a") 'yas/goto-start-of-active-field)
+(define-key yas-keymap (kbd "C-e") 'yas-goto-end-of-active-field)
+(define-key yas-keymap (kbd "C-a") 'yas-goto-start-of-active-field)
 
 ;; C-k in a field
-(defun yas/clear-current-field ()
+(defun yas-clear-current-field ()
   (interactive)
-  (let ((field (and yas/active-field-overlay
-                    (overlay-buffer yas/active-field-overlay)
-                    (overlay-get yas/active-field-overlay 'yas/field))))
-    (and field (delete-region (point) (yas/field-end field)))))
+  (let ((field (and yas--active-field-overlay
+                    (overlay-buffer yas--active-field-overlay)
+                    (overlay-get yas--active-field-overlay 'yas--field))))
+    (and field (delete-region (point) (yas--field-end field)))))
 
-(define-key yas/keymap (kbd "C-k") 'yas/clear-current-field)
+(define-key yas-keymap (kbd "C-k") 'yas-clear-current-field)
 
 (global-set-key (kbd "C-c y n") 'yas-new-snippet)
 (global-set-key (kbd "C-c y r") 'yas-reload-all)
