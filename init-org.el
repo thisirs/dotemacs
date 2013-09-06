f779a07b3a9136bcdbbd0a0313f727cb09241b7e
thisirs <thisirs@gmail.com>
2013-09-06 21:22:58 +0200

    init-org.el: Test new yasnippet workaround

--- init-org.el
+++ init-org.el
@@ -5,16 +5,11 @@
 (require 'org-drill)
 
 ;; Workaround to use yasnippet in org-mode
-(defun yas/org-very-safe-expand ()
-  (let ((yas/fallback-behavior 'return-nil)) (yas/expand)))
+(defun yas-org-very-safe-expand ()
+  (let ((yas-fallback-behavior 'return-nil))
+    (yas-expand)))
 
-(add-hook 'org-mode-hook
-          (lambda ()
-            ;; yasnippet (using the new org-cycle hooks)
-            (make-variable-buffer-local 'yas/trigger-key)
-            (setq yas/trigger-key [tab])
-            (add-to-list 'org-tab-first-hook 'yas/org-very-safe-expand)
-            (define-key yas/keymap [tab] 'yas/next-field-or-maybe-expand)))
+(add-to-list 'org-tab-first-hook 'yas-org-very-safe-expand)
 
 (setq org-todo-keywords
       '("TODO" "|" "CANCELLED" "DONE"))
