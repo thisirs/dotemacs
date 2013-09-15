ba91305fc53d45a3ceac49e506b7a4e373589e6f
thisirs <thisirs@gmail.com>
2013-09-15 19:10:32 +0200

    init-helm.el: Update helm settings

--- init-helm.el
+++ init-helm.el
@@ -18,10 +18,10 @@
 
 (setq helm-su-or-sudo "sudo")
 
-(setq helm-c-locate-command "locate -e -b %s -r \"%s\"")
+(setq helm-locate-command "locate -e -b %s -r %s")
 
 ;; Don't save history information to file
-(remove-hook 'kill-emacs-hook 'helm-c-adaptive-save-history)
+(remove-hook 'kill-emacs-hook 'helm-adaptive-save-history)
 
 ;; Make `helm-for-files-preferred-list' dynamic
 (defadvice helm-for-files (around update-helm-list activate)
@@ -30,28 +30,25 @@
     ad-do-it))
 
 (defun helm-for-files-update-list ()
-  `(helm-c-source-buffers-list
-    helm-c-source-recentf
-    helm-c-source-bookmarks
-    helm-c-source-file-cache
-    helm-c-source-files-in-current-dir
-    ,@(if (or (file-exists-p "/media/THISKEY")
-              (file-exists-p "/run/media/sylvain/THISKEY"))
-          '(helm-c-source-locate-thiskey))
-    helm-c-source-locate))
+  `(helm-source-buffers-list
+    helm-source-recentf
+    helm-source-bookmarks
+    helm-source-file-cache
+    helm-source-files-in-current-dir
+    helm-source-locate))
 
-(defun helm-c-locate-thiskey-init ()
-  "Initialize async locate process for `helm-c-source-locate'."
+(defun helm-locate-thiskey-init ()
+  "Initialize async locate process for `helm-source-locate'."
   (start-process-shell-command
    "locate-thiskey-process" nil
-   (format helm-c-locate-command
+   (format helm-locate-command
            (concat "-d " (expand-file-name "~/.locate.db") " -i")
            helm-pattern)))
 
-(defvar helm-c-source-locate-thiskey
+(defvar helm-source-locate-thiskey
   '((name . "Locate in THISKEY")
     (init . helm-locate-set-command)
-    (candidates-process . helm-c-locate-thiskey-init)
+    (candidates-process . helm-locate-thiskey-init)
     (type . file)
     (requires-pattern . 3)
     (history . ,'helm-file-name-history)
@@ -63,7 +60,7 @@
   "Find files matching the current input pattern with locate.")
 
 ;; Helm for searching manuals
-(defvar helm-c-manual-path
+(defvar helm-manual-path
   '("~/Documents/manuals/"
     ("/media/THISKEY/Documents/Manuals/"
      "~/.backup/THISKEY/Documents/Manuals/")
@@ -81,7 +78,7 @@
   string or a list of string containing fallback directories.")
 
 
-(defun helm-c-manual-path ()
+(defun helm-manual-path ()
   "Returns all existing directories containing manuals."
   (delq nil
         (mapcar
@@ -91,21 +88,21 @@
                (setq path (pop paths))
                (setq found (file-exists-p path)))
              (and found path)))
-         helm-c-manual-path)))
+         helm-manual-path)))
 
-(defvar helm-c-manual-regexp "\\.pdf\\'")
+(defvar helm-manual-regexp "\\.pdf\\'")
 
 (defun helm-manual-get-candidates ()
-  "Collect manuals found in paths `helm-c-manual-path'."
+  "Collect manuals found in paths `helm-manual-path'."
   (mapcan (lambda (path)
             (and (file-directory-p path)
-                 (directory-files path t helm-c-manual-regexp)))
-          (helm-c-manual-path)))
+                 (directory-files path t helm-manual-regexp)))
+          (helm-manual-path)))
 
-(defun helm-c-manual-transformer (files sources)
+(defun helm-manual-transformer (files sources)
   (mapcar 'file-name-nondirectory files))
 
-(defvar helm-c-source-manual
+(defvar helm-source-manual
   `((name ."Manuals")
     (candidates . helm-manual-get-candidates)
     (real-to-display . file-name-nondirectory)
@@ -113,7 +110,7 @@
 
 (defun helm-manual ()
   (interactive)
-  (helm :sources 'helm-c-source-manual
+  (helm :sources 'helm-source-manual
         :buffer "*Helm manuals*"
         :prompt "Manuals: "))
 
@@ -123,13 +120,13 @@
   '("~/Dropbox/These/bib/bregman"))
 
 ;; Open files with evince instead of xpdf
-(setq helm-c-pdfgrep-default-read-command "evince -p %p \"%f\"")
+(setq helm-pdfgrep-default-read-command "evince -p %p \"%f\"")
 
-(setq helm-c-pdfgrep-default-command "pdfgrep -C 300 --color never -niH \"%s\" %s")
+(setq helm-pdfgrep-default-command "pdfgrep -C 300 --color never -niH \"%s\" %s")
 
 (defun helm-search-bib ()
   (interactive)
-  (let ((helm-c-grep-default-function 'helm-c-pdfgrep-init))
+  (let ((helm-grep-default-function 'helm-pdfgrep-init))
     (helm-do-pdfgrep-1 helm-bib-locations)))
 
 (define-key helm-command-map (kbd "h p") 'helm-search-bib)
@@ -143,15 +140,15 @@
      ("Find file" . helm-find-many-files)
      ("Find file as root" . helm-find-file-as-root)
      ("Store org link of file" . helm-org-store-link)
-     ("Open dired in file's directory" . helm-c-open-dired)
-     ("Open file externally (C-u to choose)" . helm-c-open-file-externally)
-     ("Open file with default tool" . helm-c-open-file-with-default-tool))
+     ("Open dired in file's directory" . helm-open-dired)
+     ("Open file externally (C-u to choose)" . helm-open-file-externally)
+     ("Open file with default tool" . helm-open-file-with-default-tool))
     (persistent-help . "Show this file")
-    (action-transformer helm-c-transform-file-load-el
-                        helm-c-transform-file-browse-url)
-    (candidate-transformer helm-c-highlight-files
-                           helm-c-w32-pathname-transformer
-                           helm-c-skip-boring-files))
+    (action-transformer helm-transform-file-load-el
+                        helm-transform-file-browse-url)
+    (candidate-transformer helm-highlight-files
+                           helm-w32-pathname-transformer
+                           helm-skip-boring-files))
   "File name.")
 
 (provide 'init-helm)
