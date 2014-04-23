50c9b788dbc9863ced9eca265cdbdd0723f4f065
thisirs <thisirs@gmail.com>
2014-04-23 15:04:54 +0200

    init.el: Update google-translate settings

--- init.el
+++ init.el
@@ -224,11 +224,10 @@
 
 ;;; google translate
 (require 'google-translate)
-(global-set-key "\C-ct" 'google-translate-at-point)
-(global-set-key "\C-cT" 'google-translate-query-translate)
+(global-set-key "\C-ct" 'google-translate-smooth-translate)
 
-(setq google-translate-default-source-language "en")
-(setq google-translate-default-target-language "fr")
+(setq google-translate-translation-directions-alist
+      '(("en" . "fr") ("fr" . "en")))
 
 ;; Projectile
 (projectile-global-mode)
