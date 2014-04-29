b67ffc3820e15c831a8c3eae2e1f1339d98af069
thisirs <thisirs@gmail.com>
2014-04-29 18:38:18 +0200

    Add `uniquify-region-lines' and `uniquify-buffer-lines'

--- init-editing.el
+++ init-editing.el
@@ -117,4 +117,18 @@ cancel the indentation if needed."
                   (if (region-active-p) (region-beginning))
                   (if (region-active-p) (region-end))))
 
+;; Taken from Emacswiki
+(defun uniquify-region-lines (beg end)
+  "Remove duplicate adjacent lines in region."
+  (interactive "*r")
+  (save-excursion
+    (goto-char beg)
+    (while (re-search-forward "^\\(.*\n\\)\\1+" end t)
+      (replace-match "\\1"))))
+
+(defun uniquify-buffer-lines ()
+  "Remove duplicate adjacent lines in the current buffer."
+  (interactive)
+  (uniquify-region-lines (point-min) (point-max)))
+
 (provide 'init-editing)
