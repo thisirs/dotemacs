34a7f14bb2bbec540da43cbf5ede703078c9e539
thisirs <thisirs@gmail.com>
2014-04-30 18:40:45 +0200

    Use paredit in ielm

--- init-paredit.el
+++ init-paredit.el
@@ -3,6 +3,7 @@
 (add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
 (add-hook 'lisp-mode-hook 'enable-paredit-mode)
 (add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
+(add-hook 'ielm-mode-hook 'enable-paredit-mode)
 
 ;; From https://github.com/purcell/emacs.d.git
 ;; Use paredit in the minibuffer
