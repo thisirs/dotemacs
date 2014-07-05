a5c217f2b59d2051c6b4bf6e774741a315fe27c5
thisirs <thisirs@gmail.com>
2014-07-05 23:18:36 +0200

    Testing with no ruby settings

--- init-ruby.el
+++ init-ruby.el
@@ -1,20 +1,20 @@
-;;; Ruby
-(autoload 'inf-ruby "inf-ruby" "Run an inferior Ruby process" t)
-(autoload 'inf-ruby-setup-keybindings "inf-ruby" "" t)
+;; ;;; Ruby
+;; (autoload 'inf-ruby "inf-ruby" "Run an inferior Ruby process" t)
+;; (autoload 'inf-ruby-setup-keybindings "inf-ruby" "" t)
 
-(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+;; (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+;; (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+;; (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 
-(define-key 'help-command (kbd "R") 'yari-helm)
+;; (define-key 'help-command (kbd "R") 'yari-helm)
 
-(defun ruby-setup ()
-  ;; turn off the annoying input echo in irb
-  (setq comint-process-echoes t)
+;; (defun ruby-setup ()
+;;   ;; turn off the annoying input echo in irb
+;;   (setq comint-process-echoes t)
 
-  (define-key ruby-mode-map (kbd "RET") 'reindent-then-newline-and-indent))
+;;   (define-key ruby-mode-map (kbd "RET") 'reindent-then-newline-and-indent))
 
-(add-hook 'ruby-mode-hook 'inf-ruby-setup-keybindings)
-(add-hook 'ruby-mode-hook 'ruby-setup)
+;; (add-hook 'ruby-mode-hook 'inf-ruby-setup-keybindings)
+;; (add-hook 'ruby-mode-hook 'ruby-setup)
 
 (provide 'init-ruby)
