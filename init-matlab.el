e24f089ddc37f549ee4061d803076e1a7cd6cc06
thisirs <thisirs@gmail.com>
2014-07-08 18:15:37 +0200

    init-matlab.el: Update matlab settings

--- init-matlab.el
+++ init-matlab.el
@@ -1,19 +1,30 @@
-;;; matlab mode
-;; (autoload 'matlab-mode "matlab" "Enter MATLAB mode." t)
-;; (setq auto-mode-alist (cons '("\\.m\\'" . matlab-mode) auto-mode-alist))
-;; (autoload 'matlab-shell "matlab" "Interactive MATLAB mode." t)
+;; autoloads
 (require 'matlab-load)
-(setq matlab-shell-command-switches '("-nodesktop" "-nosplash"))
 
-(add-hook 'matlab-mode-hook
-          (lambda ()
-            (setq fill-column 76)                       ; where auto-fill should wrap
-            (turn-on-auto-fill)
-            (setq-default matlab-functions-have-end t)
-            (setq-default matlab-indent-function-body t)
-            (setq matlab-change-current-directory t)
-            (local-set-key "\M-;" 'comment-dwim)
-            (auto-fill-mode -1)
-            (setq-default matlab-indent-function t)))
+(with-eval-after-load "matlab"
+  (setq matlab-shell-command-switches '("-nodesktop" "-nosplash"))
+
+  ;; Don't switch to figure window
+  (setenv "WINDOWID" (frame-parameter (selected-frame) 'outer-window-id))
+
+  ;; From a patched version of matlab.el
+  (if (boundp 'matlab-change-current-directory)
+      (setq-default matlab-change-current-directory t))
+
+  (setq-default matlab-functions-have-end t)
+  (setq-default matlab-indent-function-body t)
+  (setq-default matlab-indent-function t)
+
+  (defun matlab-mode-preferences ()
+    (local-set-key "\M-;" 'comment-dwim)
+    (auto-fill-mode -1))
+
+  (add-hook 'matlab-mode-hook 'matlab-mode-preferences)
+
+  (defun matlab-shell-fix-slowness ()
+    (remove-hook 'comint-output-filter-functions 'matlab-shell-render-html-anchor t)
+    (remove-hook 'comint-output-filter-functions 'matlab-shell-render-errors-as-anchor t))
+
+  (add-hook 'matlab-shell-mode-hook 'matlab-shell-fix-slowness))
 
 (provide 'init-matlab)
