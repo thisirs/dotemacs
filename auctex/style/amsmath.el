78af003dc465dca840e831619234dbf90f68df44
thisirs <thisirs@gmail.com>
2014-07-09 00:03:35 +0200

    reftex-typekey-to-format-alist is no longer defined

--- auctex/style/amsmath.el
+++ auctex/style/amsmath.el
@@ -1,5 +1,5 @@
-(TeX-add-style-hook
- "amsmath"
- (lambda ()
-   (setcdr (assoc "e" reftex-typekey-to-format-alist)
-           "~\\eqref{%s}")))
+;; (TeX-add-style-hook
+;;  "amsmath"
+;;  (lambda ()
+;;    (setcdr (assoc "e" reftex-typekey-to-format-alist)
+;;            "~\\eqref{%s}")))
