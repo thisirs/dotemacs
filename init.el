5ec714d1d43867ef910ee3b1180173584b52ccec
thisirs <thisirs@gmail.com>
2014-10-28 14:00:02 +0100

    Move to melpa.org

--- init.el
+++ init.el
@@ -54,6 +54,8 @@
   (require 'package)
   (add-to-list 'package-archives
                '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  (add-to-list 'package-archives
+               '("melpa" . "http://melpa.org/packages/") t)
 
   (package-initialize)
 
