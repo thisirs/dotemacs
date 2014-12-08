159005e2f67de87387bcf4b2188970082357f50c
thisirs <thisirs@gmail.com>
2014-12-08 10:37:08 +0100

    Create parent directories in `find-file'

--- init-find-file.el
+++ init-find-file.el
@@ -43,4 +43,13 @@
             (let ((revert-without-query '(".")))
               (revert-buffer t)))))))
 
+;; Taken from http://iqbalansari.github.io/blog/2014/12/07/automatically-create-parent-directories-on-visiting-a-new-file-in-emacs/
+(defun my-create-non-existent-directory ()
+  (let ((parent-directory (file-name-directory buffer-file-name)))
+    (when (and (not (file-exists-p parent-directory))
+               (y-or-n-p (format "Directory `%s' does not exist! Create it?" parent-directory)))
+      (make-directory parent-directory t))))
+
+(add-to-list 'find-file-not-found-functions #'my-create-non-existent-directory)
+
 (provide 'init-find-file)
