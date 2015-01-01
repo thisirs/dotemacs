30a349fda4af3b97ea87d587ad00f92a6941a605
thisirs <thisirs@gmail.com>
2015-01-01 17:33:00 +0100

    Move drafts directory

--- init.el
+++ init.el
@@ -535,7 +535,7 @@ With a prefix ARG invalidates the cache first."
 
 ;; Open quickly a temporary file
 (require 'find-temp-file)
-(setq find-temp-file-directory "~/deathrow/drafts/")
+(setq find-temp-file-directory "~/CloudStation/Sylvain/drafts")
 (setq find-temp-template-default "%M/%D/%N-%T.%E")
 (add-to-list 'find-temp-template-alist (cons "m" "%M/%D/%N_%T.%E"))
 (global-set-key (kbd "C-x C-t") 'find-temp-file)
