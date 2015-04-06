1ea407b2d689cf93899206b8d4c5f7cd916d4a8b
thisirs <thisirs@gmail.com>
2015-04-06 19:19:32 +0200

    Add visit-pull-request-url

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -1,3 +1,20 @@
+;; Taken from http://endlessparentheses.com/easily-create-github-prs-from-magit.html
+(defun visit-pull-request-url ()
+  "Visit the current branch's PR on Github."
+  (interactive)
+  (browse-url
+   (format "https://github.com/%s/compare/%s"
+           (replace-regexp-in-string
+            "\\`.+github\\.com:\\(.+\\)\\.git\\'" "\\1"
+            (magit-get "remote"
+                       (magit-get-current-remote)
+                       "url"))
+           (magit-get-current-branch))))
+
+(eval-after-load 'magit
+  '(define-key magit-mode-map "V"
+     #'visit-pull-request-url))
+
 (require 'magit)
 
 (setq magit-last-seen-setup-instructions "1.4.0")
