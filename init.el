441e5df5427e89d2d202ea92481babf588b94082
thisirs <thisirs@gmail.com>
2015-08-07 16:11:58 +0200

    Do not lower frame when writing commit message

--- init.el
+++ init.el
@@ -1106,9 +1106,8 @@ case it is used in hooks."
 (add-hook 'server-done-hook 'lower-frame-unless-commit)
 
 (defun lower-frame-unless-commit ()
-  (unless (eq major-mode 'git-commit-mode)
+  (unless (and (boundp 'git-commit-mode) git-commit-mode)
     (lower-frame)))
-
 
 ;;; From http://emacs-journey.blogspot.fr/2012/06/re-builder-query-replace-this.html
 (defun reb-query-replace-this-regxp (replace)
