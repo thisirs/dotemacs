c425f73a62a191e81bdc0e91e1a6b5ef51b03b57
thisirs <thisirs@gmail.com>
2016-04-02 19:38:26 +0200

    Add DTC quotes to scratch buffer

--- lisp/init-scratch.el
+++ lisp/init-scratch.el
@@ -20,10 +20,11 @@
 (use-package scratch-message
   :config
   (defun scratch-message-random ()
-    (pcase (random 3)
+    (pcase (random 4)
       (0 (scratch-message-contrepet))
       (1 (scratch-message-SCMB))
-      (2 (scratch-message-fortune))))
+      (2 (scratch-message-DTC))
+      (3 (scratch-message-fortune))))
 
   (defun scratch-message-fortune ()
     (require 'fortune)
@@ -39,6 +40,27 @@
       (scratch-message-insert
        (with-current-buffer fortune-buffer-name
          (buffer-string)))))
+
+  (defun scratch-message-DTC ()
+    (if (executable-find "ruby")
+        (let* ((message-buffer-name "*DTC*")
+               (message-buffer (or (get-buffer message-buffer-name)
+                                   (generate-new-buffer message-buffer-name)))
+               (proc (start-process "DTC" message-buffer-name "ruby"
+                                    "/home/sylvain/CloudStation/Sylvain/scripts/DTC.rb")))
+          (with-current-buffer message-buffer-name
+            (let ((inhibit-read-only t))
+              (erase-buffer)))
+          (set-process-sentinel
+           proc
+           (lambda (process event)
+             (when (string= "finished\n" event)
+               (scratch-message-insert
+                (with-current-buffer "*DTC*"
+                  (with-fill-line-by-line
+                   (fill-region (point-min) (point-max)))
+                  (buffer-string)))))))
+      (error "Ruby is not installed")))
 
   (defun scratch-message-SCMB ()
     (if (executable-find "ruby")
