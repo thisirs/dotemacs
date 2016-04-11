56ed6c35ede4ebe6994cccca746f4a4a47197c99
thisirs <thisirs@gmail.com>
2016-04-11 23:51:30 +0200

    Simplify create-flash-binding

--- lisp/init-bindings.el
+++ lisp/init-bindings.el
@@ -98,44 +98,20 @@
 (global-set-key (kbd "C-c r") #'replace-regexp)
 
 (defmacro create-flash-binding (key)
-  "Make key `key' boundable to a complex command. Select the
-complex command by typing C-`key'. Useful for example to repeat
-an eval from M-:. Reuses the code from `repeat-complex-command'."
+  "Make KEY boundable to a command.
+
+Select the command by pressing Control + KEY. Invoke the command
+by pressing KEY."
   `(global-set-key
     (kbd ,(concat "C-" key))
     (lambda ()
       (interactive)
-      (lexical-let ((elt (nth 0 command-history))
-                    newcmd)
-        (if elt
-            (progn
-              (setq newcmd
-                    (let ((print-level nil)
-                          (minibuffer-history-position 0)
-                          (minibuffer-history-sexp-flag (1+ (minibuffer-depth))))
-                      (unwind-protect
-                          (read-from-minibuffer
-                           "Redo: " (prin1-to-string elt) read-expression-map t
-                           (cons 'command-history 0))
-
-                        ;; If command was added to command-history as a
-                        ;; string, get rid of that.  We want only
-                        ;; evaluable expressions there.
-                        (if (stringp (car command-history))
-                            (setq command-history (cdr command-history))))))
-
-              ;; If command to be redone does not match front of history,
-              ;; add it to the history.
-              (or (equal newcmd (car command-history))
-                  (setq command-history (cons newcmd command-history)))
-              (global-set-key
-               (kbd ,key)
-               (lambda ()
-                 (interactive)
-                 (message "%s" (eval newcmd)))))
-          (if command-history
-              (error "Argument %d is beyond length of command history" 0)
-            (error "There are no previous complex commands to repeat")))))))
+      (lexical-let ((cmd (read--expression ,(format "Bind %s to: " key))))
+        (global-set-key
+         (kbd ,key)
+         (lambda ()
+           (interactive)
+           (message "%s" (eval cmd))))))))
 
 (create-flash-binding "<f11>")
 (create-flash-binding "<f12>")
