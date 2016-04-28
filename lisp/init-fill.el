2bab1f2a991d3ac09bbe922f269b147c96d7dfa1
thisirs <thisirs@gmail.com>
2016-04-28 21:08:20 +0200

    Do not auto-fill in minibuffer

--- lisp/init-fill.el
+++ lisp/init-fill.el
@@ -1,8 +1,12 @@
 ;; auto-fill
 (setq comment-auto-fill-only-comments t)
 
-;; Turns on auto-fill everywhere...
-(setq-default auto-fill-function 'do-auto-fill)
+;; Turns on auto-fill everywhere except in minibuffer
+(defun do-auto-fill-exept-in-minibuffer ()
+  (unless (minibufferp)
+    (do-auto-fill)))
+
+(setq-default auto-fill-function 'do-auto-fill-exept-in-minibuffer)
 
 (global-set-key [C-return] #'comment-indent-new-line)
 
