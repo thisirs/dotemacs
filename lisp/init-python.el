56dc16438e9cda4e3acf1dd91689372cd9c1e6f0
thisirs <thisirs@gmail.com>
2018-02-08 13:05:40 +0100

    python3 in elpy, no flymake but flycheck

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,6 +1,13 @@
 (use-package elpy
   :ensure
   :config
-  (elpy-enable))
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (setq python-indent-guess-indent-offset-verbose nil)
+  (elpy-enable)
+  (setq python-shell-interpreter "ipython3"
+        python-shell-interpreter-args "-i --simple-prompt")
+
+  (when (use-package flycheck)
+    (add-hook 'elpy-mode-hook 'flycheck-mode)))
 
 (provide 'init-python)
