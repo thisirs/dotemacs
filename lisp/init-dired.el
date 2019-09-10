ab87b5ed5c660a419339277fa283f0d6639d73b3
thisirs <thisirs@gmail.com>
2019-09-10 18:01:40 +0200

    use-packagify dired

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -1,21 +1,111 @@
-(require 'dired-x)
+(use-package dired
+  :straight nil
+  :init
+  (defun dired-copy-filename-as-kill-fix (&optional arg)
+    (interactive "P")
+    (cond ((equal arg '(4))
+           (dired-copy-filename-as-kill '(0)))
+          ((equal arg '(16))
+           (let* ((file (car (dired-get-marked-files nil t)))
+                  (root (vc-responsible-backend-root file))
+                  (string (file-relative-name file root)))
+             (kill-new string)
+             (message "%s" string)))
+          (t (dired-copy-filename-as-kill arg))))
 
-(setq dired-recursive-copies 'always)
-(setq dired-dwim-target t)
+  ;; Adapted from http://stackoverflow.com/a/19112313/1299368
+  (defun dired-ediff-marked-files ()
+    "Run ediff-files on a pair of files marked in dired buffer"
+    (interactive)
+    (let* ((marked-files (dired-get-marked-files))
+           (other-win (get-window-with-predicate
+                       (lambda (window)
+                         (with-current-buffer (window-buffer window)
+                           (and (not (eq window (selected-window)))
+                                (eq major-mode 'dired-mode))))))
+           (other-marked-files (and other-win
+                                    (with-current-buffer (window-buffer other-win)
+                                      (dired-get-marked-files nil)))))
+      (cond ((= (length marked-files) 2)
+             (let ((invert (if (apply #'file-newer-than-file-p marked-files)
+                               'nreverse
+                             'identity)))
+               (apply 'ediff-files (funcall invert marked-files))))
+            ((and (= (length marked-files) 1)
+                  (= (length other-marked-files) 1))
+             (if (file-newer-than-file-p (car marked-files)
+                                         (car other-marked-files))
+                 (ediff-files (car other-marked-files)
+                              (car marked-files))
+               (ediff-files (car marked-files)
+                            (car other-marked-files))))
+            ((= (length marked-files) 1)
+             (read-file-name "file: " (dired-dwim-target-directory)))
+            (t (error "mark exactly 2 files, at least 1 locally")))))
 
-(setq dired-listing-switches "-alGh --group-directories-first")
+  (defun dired-open (&optional file-list)
+    (interactive
+     (list (dired-get-marked-files t current-prefix-arg)))
+    (apply 'call-process "xdg-open" nil 0 nil file-list))
 
-;; Auto-revert mode
-(add-hook 'dired-mode-hook #'turn-on-auto-revert-mode)
-(setq auto-revert-verbose nil)
+  (defmacro dired-define-sorter (symbol switches doc)
+    `(defun ,symbol ()
+       ,doc
+       (interactive)
+       (dired-sort-other
+        (concat dired-listing-switches
+                (unless (string-match "-r" dired-actual-switches)
+                  " -r")
+                ,switches))))
 
-;; Auto-revert dired buffer on revisiting
-(setq dired-auto-revert-buffer t)
+  (dired-define-sorter dired-sort-by-name "" "Sort by name")
+  (dired-define-sorter dired-sort-by-extension " -X" "Sort by extension")
+  (dired-define-sorter dired-sort-by-size " -S" "Sort by size")
+  (dired-define-sorter dired-sort-by-time " -t" "Sort by time")
+  (dired-define-sorter dired-sort-directory-first " --group-directories-first" "Sort directories first")
 
-;; Make dired less verbose
-(with-emacs-version> "24.3.90"
+  :bind (:map dired-mode-map
+              :prefix-map dired-sort-map
+              :prefix "s"
+              ("x" . dired-sort-by-extension)
+              ("s" . dired-sort-by-size)
+              ("t" . dired-sort-by-time)
+              ("n" . dired-sort-by-name)
+              ("d" . dired-sort-directory-first)
+              :map dired-mode-map
+              ("C-j" . dired-up-directory)
+              ("<C-return>" . dired-open)
+              ("e" . dired-ediff-marked-files)
+              ("M-!" . async-shell-command)
+              ("C-j" . dired-up-directory)
+              ("C-x C-k" . dired-do-delete)
+              ("w" . dired-copy-filename-as-kill-fix)
+              ("@" . (lambda () (interactive) (dired "/tmp")))
+              ("/" . (lambda () (interactive) (dired "/")))
+              ("~" . (lambda () (interactive) (dired (getenv "HOME")))))
+
+  :config
+  (setq dired-recursive-copies 'always)
+  (setq dired-dwim-target t)
+  (add-hook 'dired-mode-hook #'turn-on-auto-revert-mode)
+
+
+  (setq dired-clean-confirm-killing-deleted-buffers nil)
+
+  ;; Automatically revert Dired buffer on revisiting.
+  (setq dired-auto-revert-buffer t)
+
+  ;; Hide details in dired listing
   (add-hook 'dired-mode-hook #'dired-hide-details-mode)
+
+  ;; But show symlinks
   (setq dired-hide-details-hide-symlink-targets nil))
+
+(use-package dired-x
+  :straight nil
+  :bind (("C-x C-j" . dired-jump))
+  :config)
+
 
 (defun dired-bury-all (&optional kill)
   "Bury dired buffer as they appear. It keeps you from pressing
@@ -29,177 +119,76 @@ repeatedly q."
                   (or (quit-window kill) t)))))
 
 ;; Don't wait for auto-revert and reload dired after creating a directory
-(advice-add 'dired-create-directory :after (lambda (&rest args) (revert-buffer)))
+;; (advice-add 'dired-create-directory :after (lambda (&rest args) (revert-buffer)))
 
-(defvar dired-sort-map
-  (let ((map (make-sparse-keymap)))
-    (mapc
-     (lambda (elt)
-       (define-key map (car elt)
-         `(lambda ()
-            (interactive)
-            (dired-sort-other
-             (concat dired-listing-switches
-                     (unless (string-match "-r" dired-actual-switches)
-                       " -r") ,(cadr elt))))))
-     '(("n" "")
-       ("x" " -X")
-       ("s" " -S")
-       ("t" " -t")
-       ("d" " --group-directories-first")))
-    map))
+;; (define-key dired-mode-map (kbd "Q") #'dired-bury-all)
 
-(define-key dired-mode-map (kbd "Q") #'dired-bury-all)
+;; ;; C-c C-m C-a jumps to gnus with current file attached
+;; (add-hook 'dired-mode-hook #'turn-on-gnus-dired-mode)
 
-(defun dired-open (&optional file-list)
-  (interactive
-   (list (dired-get-marked-files t current-prefix-arg)))
-  (apply 'call-process "xdg-open" nil 0 nil file-list))
+;; (defvar dired-do-command-result nil)
 
-(define-key dired-mode-map (kbd "<C-return>") #'dired-open)
-(define-key dired-mode-map "e" #'dired-ediff-marked-files)
+;; (defun dired-do-command (command &optional reduce)
+;;   "Run COMMAND on marked files. Any files not already open will be opened.
+;; After this command has been run, any buffers it's modified will remain
+;; open and unsaved."
+;;   (interactive (list (read--expression "Command: ")
+;;                      (read--expression "Reduce return values (default list): "
+;;                                        "list")))
+;;   (unless reduce (setq reduce 'list))
+;;   (setq dired-do-command-result
+;;         (apply reduce
+;;                (mapcar
+;;                 (lambda (filename)
+;;                   (save-window-excursion
+;;                     (switch-to-buffer (find-file-noselect filename))
+;;                     (if (symbolp command)
+;;                         (call-interactively command)
+;;                       (eval command))))
+;;                 (dired-get-marked-files nil nil 'dired-nondirectory-p)))))
 
-(define-key dired-mode-map (kbd "M-!") #'async-shell-command)
+;; (defmacro with-current-value (variable buffers &rest body)
+;;   "Execute the forms in BODY with VARIABLE temporarily set to
+;; current in all the buffers BUFFERS. The value returned is the
+;; value of the last form in BODY."
+;;   (declare (indent 2))
+;;   (let ((current-variable (make-symbol "current-variable"))
+;;         (saved-variables (make-symbol "saved-variables")))
+;;     `(let* ((,current-variable ,variable)
+;;             (,saved-variables
+;;              (mapcar (lambda (buf)
+;;                        (list buf (local-variable-p ',variable buf)
+;;                              (buffer-local-value ',variable buf)))
+;;                      ,buffers)))
+;;        (mapc (lambda (buf)
+;;                (with-current-buffer buf
+;;                  (setq ,variable ,current-variable)))
+;;              ,buffers)
+;;        (prog1 (progn ,@body)
+;;          ;; restore variable value
+;;          (mapc (lambda (e)
+;;                  (with-current-buffer (car e)
+;;                    (if (cadr e)
+;;                        (setq ,variable (caddr e))
+;;                      (kill-local-variable ',variable))))
+;;                ,saved-variables)))))
 
-(define-key dired-mode-map "s" dired-sort-map)
+;; (defun dired-do-occur (regexp &optional nlines)
+;;   "View lines which match REGEXP in all marked buffers.
+;; Optional argument NLINES says how many lines of context to display: it
+;; defaults to one. "
+;;   (interactive (occur-read-primary-args))
+;;   (if (or (not (integerp nlines))
+;;           (< nlines 0))
+;;       (setq nlines 0))
+;;   (let ((marked-buffers (mapcar 'find-file-noselect (dired-get-marked-files))))
+;;     (with-current-value case-fold-search marked-buffers
+;;       (occur-1 regexp nlines marked-buffers))))
 
-;; Delete with C-x C-k to match file buffers and magit
-(define-key dired-mode-map (kbd "C-x C-k") #'dired-do-delete)
-
-;; Quick jump to root, user dir or /tmp
-(define-key dired-mode-map (kbd "@")
-  (lambda ()
-    (interactive)
-    (dired "/tmp")))
-
-(define-key dired-mode-map (kbd "/")
-  (lambda ()
-    (interactive)
-    (dired "/")))
-
-(define-key dired-mode-map (kbd "~")
-  (lambda ()
-    (interactive)
-    (dired (getenv "HOME"))))
-
-(defun dired-copy-filename-as-kill-fix (&optional arg)
-  (interactive "P")
-  (cond ((equal arg '(4))
-         (dired-copy-filename-as-kill '(0)))
-        ((equal arg '(16))
-         (let* ((file (car (dired-get-marked-files nil t)))
-                (root (vc-responsible-backend-root file))
-                (string (file-relative-name file root)))
-           (kill-new string)
-           (message "%s" string)))
-        (t (dired-copy-filename-as-kill arg))))
-
-(define-key dired-mode-map (kbd "w") #'dired-copy-filename-as-kill-fix)
-
-;; C-c C-m C-a jumps to gnus with current file attached
-(add-hook 'dired-mode-hook #'turn-on-gnus-dired-mode)
-
-(eval-after-load "dired-aux"
-  '(add-to-list 'dired-compress-file-suffixes
-                '("\\.zip\\'" ".zip" "unzip")))
-
-(defun dired-nondirectory-p (file)
-  (not (file-directory-p file)))
-
-(defvar dired-do-command-result nil)
-
-(defun dired-do-command (command &optional reduce)
-  "Run COMMAND on marked files. Any files not already open will be opened.
-After this command has been run, any buffers it's modified will remain
-open and unsaved."
-  (interactive (list (read--expression "Command: ")
-                     (read--expression "Reduce return values (default list): "
-                                       "list")))
-  (unless reduce (setq reduce 'list))
-  (setq dired-do-command-result
-        (apply reduce
-               (mapcar
-                (lambda (filename)
-                  (save-window-excursion
-                    (switch-to-buffer (find-file-noselect filename))
-                    (if (symbolp command)
-                        (call-interactively command)
-                      (eval command))))
-                (dired-get-marked-files nil nil 'dired-nondirectory-p)))))
-
-(defmacro with-current-value (variable buffers &rest body)
-  "Execute the forms in BODY with VARIABLE temporarily set to
-current in all the buffers BUFFERS. The value returned is the
-value of the last form in BODY."
-  (declare (indent 2))
-  (let ((current-variable (make-symbol "current-variable"))
-        (saved-variables (make-symbol "saved-variables")))
-    `(let* ((,current-variable ,variable)
-            (,saved-variables
-             (mapcar (lambda (buf)
-                       (list buf (local-variable-p ',variable buf)
-                             (buffer-local-value ',variable buf)))
-                     ,buffers)))
-       (mapc (lambda (buf)
-               (with-current-buffer buf
-                 (setq ,variable ,current-variable)))
-             ,buffers)
-       (prog1 (progn ,@body)
-         ;; restore variable value
-         (mapc (lambda (e)
-                 (with-current-buffer (car e)
-                   (if (cadr e)
-                       (setq ,variable (caddr e))
-                     (kill-local-variable ',variable))))
-               ,saved-variables)))))
-
-(defun dired-do-occur (regexp &optional nlines)
-  "View lines which match REGEXP in all marked buffers.
-Optional argument NLINES says how many lines of context to display: it
-defaults to one. "
-  (interactive (occur-read-primary-args))
-  (if (or (not (integerp nlines))
-          (< nlines 0))
-      (setq nlines 0))
-  (let ((marked-buffers (mapcar 'find-file-noselect (dired-get-marked-files))))
-    (with-current-value case-fold-search marked-buffers
-      (occur-1 regexp nlines marked-buffers))))
-
-;; Adapted from http://stackoverflow.com/a/19112313/1299368
-(defun dired-ediff-marked-files ()
-  "Run ediff-files on a pair of files marked in dired buffer"
-  (interactive)
-  (let* ((marked-files (dired-get-marked-files))
-         (other-win (get-window-with-predicate
-                     (lambda (window)
-                       (with-current-buffer (window-buffer window)
-                         (and (not (eq window (selected-window)))
-                              (eq major-mode 'dired-mode))))))
-         (other-marked-files (and other-win
-                                  (with-current-buffer (window-buffer other-win)
-                                    (dired-get-marked-files nil)))))
-    (cond ((= (length marked-files) 2)
-           (let ((invert (if (apply #'file-newer-than-file-p marked-files)
-                             'nreverse
-                           'identity)))
-             (apply 'ediff-files (funcall invert marked-files))))
-          ((and (= (length marked-files) 1)
-                (= (length other-marked-files) 1))
-           (if (file-newer-than-file-p (car marked-files)
-                                       (car other-marked-files))
-               (ediff-files (car other-marked-files)
-                            (car marked-files))
-             (ediff-files (car marked-files)
-                          (car other-marked-files))))
-          ((= (length marked-files) 1)
-           (read-file-name "file: " (dired-dwim-target-directory)))
-          (t (error "mark exactly 2 files, at least 1 locally")))))
-
-(use-package dired-ranger
-  :straight t
-  :bind (:map dired-mode-map
-              ("W" . dired-ranger-copy)
-              ("X" . dired-ranger-move)
-              ("Y" . dired-ranger-paste)))
+;; (use-package dired-ranger
+;;   :bind (:map dired-mode-map
+;;               ("W" . dired-ranger-copy)
+;;               ("X" . dired-ranger-move)
+;;               ("Y" . dired-ranger-paste)))
 
 (provide 'init-dired)
