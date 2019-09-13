3c68d80b6f38057fcd7193fcb9ec09e25b595a74
thisirs <thisirs@gmail.com>
2019-09-13 16:52:45 +0200

    Break down ess in separate packages (ess-site might not be loaded)

--- lisp/init-ess.el
+++ lisp/init-ess.el
@@ -1,58 +1,66 @@
-(use-package julia-mode
-  :straight (julia-mode :repo "JuliaEditorSupport/julia-emacs"
-                        :fetcher github
-                        :files "*.el"))
+;; (use-package julia-mode
+;;   :straight (julia-mode :repo "JuliaEditorSupport/julia-emacs"
+;;                         :fetcher github
+;;                         :files "*.el"))
+(use-package ess)
 
-;; http://ess.r-project.org
-(use-package ess-site                   ; Emacs Speaks Statistics
-  :straight ess
-  :bind (:map ess-r-mode-map ("_" . ess-insert-assign)
-              :map inferior-ess-mode-map ("_" . ess-insert-assign))
-  ;; No special behaviour of comments starting with #, ## or ###
-  :custom (ess-indent-with-fancy-comments nil)
+(use-package ess-r-mode
+  :straight nil
+  :bind (:map ess-r-mode-map ("_" . ess-insert-assign))
   :config
   ;; No double sharp sign when starting a comment
   (setq ess-r-customize-alist
-        (append ess-r-customize-alist '((comment-add . 0))))
+        (append ess-r-customize-alist '((comment-add . 0)))))
 
-  (defun tidy-Rtex-chunks ()
-    "Tidy all the R chunks delimited by begin.rcode/end.rcode."
-    (interactive)
-    (save-excursion
-      (goto-char (point-min))
-      (let ((re-begin-chunk "^ *\\(%+\\) *begin\\.rcode *")
-            (re-end-chunk "end\\.rcode")
-            (re-prefix-chunk " *%+ *")
-            (prefix-chunk "% "))
-        (while (re-search-forward begin-chunk nil t)
-          (let* ((column (progn
-                           (goto-char (match-beginning 1))
-                           (current-column)))
-                 (beg (progn
-                        (forward-line 1)
-                        (point-at-bol)))
-                 (end (progn
-                        (re-search-forward end-chunk nil t)
-                        (forward-line -1)
-                        (point-at-eol)))
-                 (code (delete-and-extract-region beg end))
-                 (new-code (progn
-                             (with-temp-buffer
-                               (insert code)
-                               (goto-char (point-min))
-                               (while (re-search-forward re-prefix-chunk nil t)
-                                 (replace-match ""))
-                               (write-file (make-temp-file "foo"))
-                               (tidy-R-buffer nil nil "indent = 2, arrow = TRUE, width.cutoff = 500")
-                               (goto-char (point-min))
-                               (while (re-search-forward "^\\(.\\)" nil t)
-                                 (replace-match (concat
-                                                 (make-string column ?\ )
-                                                 prefix-chunk
-                                                 "\\1")))
-                               (save-buffer)
-                               (string-trim-right (buffer-string))))))
-            (goto-char beg)
-            (insert new-code)))))))
+(use-package ess-inf
+  :straight nil
+  :bind (:map inferior-ess-mode-map ("_" . ess-insert-assign)))
+
+
+(use-package ess-custom
+  :straight nil
+  ;; No special behaviour of comments starting with #, ## or ###
+  :custom (ess-indent-with-fancy-comments nil))
+
+;; http://ess.r-project.org
+(defun tidy-Rtex-chunks ()
+  "Tidy all the R chunks delimited by begin.rcode/end.rcode."
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (let ((re-begin-chunk "^ *\\(%+\\) *begin\\.rcode *")
+          (re-end-chunk "end\\.rcode")
+          (re-prefix-chunk " *%+ *")
+          (prefix-chunk "% "))
+      (while (re-search-forward begin-chunk nil t)
+        (let* ((column (progn
+                         (goto-char (match-beginning 1))
+                         (current-column)))
+               (beg (progn
+                      (forward-line 1)
+                      (point-at-bol)))
+               (end (progn
+                      (re-search-forward end-chunk nil t)
+                      (forward-line -1)
+                      (point-at-eol)))
+               (code (delete-and-extract-region beg end))
+               (new-code (progn
+                           (with-temp-buffer
+                             (insert code)
+                             (goto-char (point-min))
+                             (while (re-search-forward re-prefix-chunk nil t)
+                               (replace-match ""))
+                             (write-file (make-temp-file "foo"))
+                             (tidy-R-buffer nil nil "indent = 2, arrow = TRUE, width.cutoff = 500")
+                             (goto-char (point-min))
+                             (while (re-search-forward "^\\(.\\)" nil t)
+                               (replace-match (concat
+                                               (make-string column ?\ )
+                                               prefix-chunk
+                                               "\\1")))
+                             (save-buffer)
+                             (string-trim-right (buffer-string))))))
+          (goto-char beg)
+          (insert new-code))))))
 
 (provide 'init-ess)
