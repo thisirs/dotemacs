a296e6a4d7008ffb83e9f691f8388f42cc83b695
thisirs <thisirs@gmail.com>
2020-01-08 09:27:14 +0100

    Don't auto byte-recompile

--- lisp/init-elisp.el
+++ lisp/init-elisp.el
@@ -99,14 +99,14 @@
       (error (goto-char opoint)
              (message "eval-and-replace: %s" error)))))
 
-(defun auto-byte-recompile ()
-  "If the current buffer is in emacs-lisp-mode and there already exists an `.elc'
-file corresponding to the current buffer file, then recompile the file."
-  (interactive)
-  (when (and (eq major-mode 'emacs-lisp-mode)
-             (file-exists-p (byte-compile-dest-file buffer-file-name)))
-    (byte-compile-file buffer-file-name)))
+;; (defun auto-byte-recompile ()
+;;   "If the current buffer is in emacs-lisp-mode and there already exists an `.elc'
+;; file corresponding to the current buffer file, then recompile the file."
+;;   (interactive)
+;;   (when (and (eq major-mode 'emacs-lisp-mode)
+;;              (file-exists-p (byte-compile-dest-file buffer-file-name)))
+;;     (byte-compile-file buffer-file-name)))
 
-(add-hook 'after-save-hook #'auto-byte-recompile)
+;; (add-hook 'after-save-hook #'auto-byte-recompile)
 
 (provide 'init-elisp)
