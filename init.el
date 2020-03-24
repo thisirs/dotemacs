2c02c72932c53ff3cf811791c974ab325d32f215
thisirs <thisirs@gmail.com>
2020-03-24 18:39:44 +0100

    Testing a better keyboard-quit

--- init.el
+++ init.el
@@ -1468,6 +1468,44 @@ corresponding statement."
 (use-package robe
   :hook (ruby-mode . robe-mode))
 
+(use-package simple
+  :straight nil
+  :preface
+  ;; From https://with-emacs.com/posts/tips/quit-current-context/
+  (defun keyboard-quit-context+ ()
+    "Quit current context.
+
+This function is a combination of `keyboard-quit' and
+`keyboard-escape-quit' with some parts omitted and some custom
+behavior added."
+    (interactive)
+    (cond ((region-active-p)
+           ;; Avoid adding the region to the window selection.
+           (setq saved-region-selection nil)
+           (let (select-active-regions)
+             (deactivate-mark)))
+          ((eq last-command 'mode-exited) nil)
+          (current-prefix-arg
+           nil)
+          (defining-kbd-macro
+            (message
+             (substitute-command-keys
+              "Quit is ignored during macro defintion, use \\[kmacro-end-macro] if you want to stop macro definition"))
+            (cancel-kbd-macro-events))
+          ((active-minibuffer-window)
+           (when (get-buffer-window "*Completions*")
+             ;; hide completions first so point stays in active window when
+             ;; outside the minibuffer
+             (minibuffer-hide-completions))
+           (abort-recursive-edit))
+          (t
+           (when completion-in-region-mode
+             (completion-in-region-mode -1))
+           (let ((debug-on-quit nil))
+             (signal 'quit nil)))))
+
+  :bind ([remap keyboard-quit] . #'keyboard-quit-context+))
+
 ;; Smart modeline
 ;; http://github.com/Malabarba/smart-mode-line
 (use-package smart-mode-line            ; A color coded smart mode-line.
