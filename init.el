877426354ff5aa5e6b4332aa0e857a94232db53d
thisirs <thisirs@gmail.com>
2020-11-14 16:59:50 +0100

    Whichkey on C-h only

--- init.el
+++ init.el
@@ -1849,6 +1849,9 @@ behavior added."
   :demand
   :diminish
   :config
+  (setq which-key-show-early-on-C-h t)
+  (setq which-key-idle-delay 10000)
+  (setq which-key-idle-secondary-delay 0.05)
   (which-key-mode))
 
 (use-package whitespace
