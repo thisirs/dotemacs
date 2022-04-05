ff4fcc5912593511ff0953f62b31fa2e018143f6
thisirs <thisirs@gmail.com>
2022-04-05 15:10:54 +0200

    Update mu4e settings

--- init.el
+++ init.el
@@ -1282,6 +1282,7 @@ the vertical drag is done."
   :custom
   (mu4e-attachment-dir "~/deathrow")
   (mu4e-completing-read-function 'completing-read)
+  (mu4e-save-multiple-attachments-without-asking t)
   (mu4e-compose-keep-self-cc nil)
   (mu4e-compose-dont-reply-to-self t)
   (mu4e-context-policy 'pick-first)
