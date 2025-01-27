4010241855f081455a550d107fbc2942a9c86cd9
thisirs <thisirs@gmail.com>
2025-01-27 11:50:51 +0100

    Don't use q anymore to quit transient

--- init.el
+++ init.el
@@ -2817,12 +2817,7 @@ behavior added."
 (use-package tmr)                       ; Set timers using a convenient notation
 
 ;; https://github.com/magit/transient
-(use-package transient                  ; Transient commands
-  :config
-  ;; Allow using `q' to quit out of popups, in addition to `C-g'. See
-  ;; <https://magit.vc/manual/transient.html#Why-does-q-not-quit-popups-anymore_003f>
-  ;; for discussion.
-  (transient-bind-q-to-quit))
+(use-package transient)                 ; Transient commands
 
 (use-package transpose-frame            ; Transpose windows arrangement in a frame
   :bind ("<C-kp-multiply>" . rotate-frame-anticlockwise))
